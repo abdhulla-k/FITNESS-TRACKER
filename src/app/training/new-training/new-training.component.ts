fb140560e2790bb2e952b5cc9f3f85868d2f293a
abdhulla-k <abdhullak149@gmail.com>
2022-07-11 15:44:59 +0530

    added available exercises from fireStore in an another way

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { TrainingService } from '../training.service';
 
@@ -19,10 +20,26 @@ export class NewTrainingComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    // this.exercises = this.trainingService.getAvailableExercises()
+    // this.exercises = this.db
+    //   .collection( 'availableExercises' )
+    //   .valueChanges();
+
     this.exercises = this.db
-      .collection( 'availableExercises' )
-      .valueChanges();
+      .collection( 'availableExercises')
+      .snapshotChanges()
+      .pipe(
+        map( docData => {
+          return docData.map( doc => {
+            let object1 = doc.payload.doc.data()
+
+            return {
+              id: doc.payload.doc.id,
+              ...( typeof object1 === 'object' )? object1 : {}
+            };
+
+          });
+        })
+      )
   }
 
   onStartTraining( form: NgForm ) {
